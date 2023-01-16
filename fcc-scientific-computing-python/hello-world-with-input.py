cd86fdcb69bc4704141a6f2af4e4328d1882a55f
Lucas Prochnow <lucas.prochnow@meliuz.com.br>
2023-01-16 19:54:38 -0300

    feat: Add first python3 practices

---
+++ fcc-scientific-computing-python/hello-world-with-input.py
@@ -0,0 +1,7 @@
+userName = input("What's your name? ")
+print('Hello', userName)
+
+# This is a comment!
+europeFloor = input('Now ' + userName + ', type the Europe elevator floor... ')
+usFloor = int(europeFloor) + 1
+print('US floor is', usFloor)
---
+++ fcc-scientific-computing-python/types-conversion.py
@@ -0,0 +1,8 @@
+stringVal = '123'
+print(type(stringVal))
+
+intVal = int(stringVal)
+print(type(intVal))
+
+newStrignVal = str(stringVal)
+print(type(newStrignVal))
